edf3b1b0f9a5ee798c3336efa6a502983f483c5f
Rea Thye <d.nikkit2012@gmail.com>
2021-06-17 21:36:02 +0300

    logout

--- src/main/java/com/example/mywebshop/config/SecurityConfig.java
+++ src/main/java/com/example/mywebshop/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -35,7 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                     .loginPage("/login")
-                    .successHandler(successHandler);
+                    .successHandler(successHandler)
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"));
     }
 
     @Bean
