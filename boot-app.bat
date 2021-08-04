ff1f89bed4084596345db8bf1564eef1286cb0fa
Rea Thye <d.nikkit2012@gmail.com>
2021-08-04 20:45:55 +0300

    moved files

--- boot-app.bat
+++
@@ -1,4 +0,0 @@
-call mvnw clean package -DskipTests
-call cd docker/
-call docker-compose build --no-cache
-call docker-compose up
--- clear.bat
+++
@@ -1,2 +0,0 @@
-call docker rm -f my-db-app db
-call docker image rm -f db-management-app
